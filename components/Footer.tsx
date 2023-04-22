b2aaa4b03934303dbfa8fcb9d5c96e3e598c9b64
NANDIMANDALAM-SUNNY <r170911@rguktrkv.ac.in>
2023-04-22 15:31:57 +0530

    footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -14,7 +14,7 @@ const Footer = () => {
         <a href='//linkedin.com/in/nandimandalamsunny2809/' target="_blank" rel='noopener noreferrer'><FaLinkedin style={{margin:"0 20px 0 20px"}} size="50px" color="skyblue"/></a>
         <a href='//t.me/sunny170911' target="_blank" rel='noopener noreferrer'><FaTelegram style={{margin:"0 20px 0 20px"}} size="50px" color="lightgreen"/></a>
         <a href='//github.com/NANDIMANDALAM-SUNNY' target="_blank" rel='noopener noreferrer'><FaGithub style={{margin:"0 20px 0 20px"}} size="50px" color="lightpink"/></a> 
-        <a href='//n.sunny.netlify.app' target="_blank" rel='noopener noreferrer'><CgWebsite style={{margin:"0 20px 0 20px"}} size="50px" color="lightslategray"/></a>
+       
         </div> 
         <div>
         <p className="m-0 text-center text-white mt-4">
